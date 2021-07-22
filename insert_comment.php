e0cd7334ca859740c81db784a197ca508f798580
justasd3 <61118821+justasd3@users.noreply.github.com>
2021-07-22 15:29:04 +0300

    Update insert_comment.php

--- insert_comment.php
+++ insert_comment.php
@@ -1,12 +1,11 @@
 <?php
 
-$connect = new PDO('mysql:host=sql210.epizy.com;dbname=epiz_29199337_orcaphpdatabase', 'epiz_29199337', 'd5F7f2VdQTB8Mm');
+$connect = new PDO('mysql:host=sql210.epizy.com;dbname=epiz_29199337_orcaphpdatabase;charset=utf8', 'epiz_29199337', 'd5F7f2VdQTB8Mm');
 
 $error = '';
 $name = '';
 $comment_data = '';
 $email = '';
-
 
 if(empty($_POST["name"]))
 {
@@ -14,13 +13,14 @@ if(empty($_POST["name"]))
 }
 else
 {
- $name = $_POST["name"];
+   $name = $_POST["name"];
+ //$name = mysqli_real_escape_string($connect ,$_POST["name"]);
 }
 
 if(empty($_POST["comment_data"]))
 {
  $error .= '<p class="text-danger">Comment is required</p>';
-}
+} 
 else
 {
  $comment_data = $_POST["comment_data"];
@@ -39,25 +39,35 @@ if($error == '')
 {
  $query = "
  INSERT INTO comments
- (father_id, name, comment, email) 
- VALUES (:father_id, :name, :comment, :email)
+ (father_id, name, comment, email, created_at) 
+ VALUES (:father_id, :name, :comment, :email, :created_at)
  ";
+ console.log("asd");
  $statement = $connect->prepare($query);
+
+$statement->bindValue(':father_id', $_POST["id"]);
+$statement->bindValue(':name', $name, PDO::PARAM_STR);
+$statement->bindValue(':comment', $comment_data, PDO::PARAM_STR);
+$statement->bindValue(':email', $email, PDO::PARAM_STR);
+
+//  $statement->execute();
+
+
  $statement->execute(
   array(
    ':father_id' => $_POST["id"],
    ':comment'    => $comment_data,
    ':name' => $name,
    ':email' => $email,
+   ':created_at' => date("Y-m-d"),
   )
  );
  $error = '<label class="text-success">Comment Added</label>';
 }
 
 $data = array(
+    
  'error'  => $error
 );
-
 echo json_encode($data);
-
 ?>
